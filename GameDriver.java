d9279d6c38dd3c9e31a005261fc13b4aa25d3cf1
TimatoPaste <70876288+TimatoPaste@users.noreply.github.com>
2022-12-08 15:15:16 -0800

    Diagnosis and error fixes

--- GameDriver.java
+++ GameDriver.java
@@ -20,7 +20,9 @@ public class GameDriver implements InputControl, InputKeyControl{
 	make moves
 	*/
 	static Player current;
+	String nextMove;
 	public void play(){
+		System.out.println("played");
 		if(state.isGameover()){ //checks for winner or a draw
 			System.out.println(state);
 			if(state.getWinner()!=null){
@@ -35,57 +37,63 @@ public class GameDriver implements InputControl, InputKeyControl{
 		
 		current = state.getCurrentPlayer();
 		System.out.println("current player is "+current);
-		String nextMove = current.getNextMove(state);
+		nextMove = "no move";
+		
+		
+		//---------------------NEED TO IMPLEMENT A WAY TO WAIT FOR THE CLICK TO HAPPEN AND THEN MOVE ON
+		
+		
+		
+		
+		
+		
 		
 		if(nextMove.equals("no move")){
 			System.out.println("Player "+current+" has no move");
 		}
 		else{
 			System.out.println("\n"+current.getName() +" makes move "+nextMove+"\n");
+			state.makeMove(nextMove);
 		}
-		state.makeMove(nextMove);
 	}
+	static GameDriver driver;
 	static Ttt game;
 	public static void main(String[] args){
 		KeyController kC = new KeyController(Canvas.getInstance(),new GameDriver());
 		MouseController mC = new MouseController(Canvas.getInstance(),new GameDriver());
 		game = new Ttt();
-		new GameDriver(game).play();
-		System.out.println("\nplay again? y/n");
+		driver = new GameDriver(game);
+		driver.play();
+		/*System.out.println("\nplay again? y/n");*/
 		//used to be the most beautiful line of code but tim killed it
 		//if(new Scanner(System.in).nextLine().trim().toUpperCase().contains("Y")) main(args);
-		if(new Scanner(System.in).nextLine().trim().toUpperCase().contains("Y")){
+		/*if(new Scanner(System.in).nextLine().trim().toUpperCase().contains("Y")){
 			System.out.print("\n");
 			main(args);
 		}
-		else System.out.println("\nSO LONG");
+		else System.out.println("\nSO LONG");*/
 	}
 	public void onMouseClick(double x, double y) {
 		
 		
 		y -= 30;
 		Ttt.Square apple = Ttt.whichSquare(x,y);
-		if(apple != null){
-			game.makeMove(apple.getRow()+" "+apple.getCol());
-		}
-		else{
-			
-		}
-		Ttt.Square[][] temp = game.getState();
+		
+		// Ttt.Square[][] temp = game.getState();
 		
 		//text outputs the board
-		for(int r = 0;r<temp.length;r++){
+		/*for(int r = 0;r<temp.length;r++){
 			for(int c = 0;c<temp[0].length;c++){
 				System.out.print(temp[r][c]+" ");
-				current.nextMove = temp[r][c];
+				// current.nextMove = temp[r][c];
 				synchronized(this){
 					notifyAll();
 				}
 			}
 			System.out.print("\n");
-		}
-		
-		System.out.println(game.isGameover());
+		}*/
+		nextMove = apple.getRow()+" "+apple.getCol();
+		driver.play();
 	}
 	
 	public void keyPress(String s) {
