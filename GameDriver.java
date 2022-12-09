4a338d2bf8b7a86820590c79ec9d9b4a76dca870
TimatoPaste <70876288+TimatoPaste@users.noreply.github.com>
2022-12-08 19:26:04 -0800

    u

--- GameDriver.java
+++ GameDriver.java
@@ -1,7 +1,6 @@
 import pkg.*;
 import java.util.Scanner;
 
-
 public class GameDriver implements InputControl, InputKeyControl{
 	
 	private GameState state;
@@ -41,9 +40,12 @@ public class GameDriver implements InputControl, InputKeyControl{
 		
 		
 		//---------------------NEED TO IMPLEMENT A WAY TO WAIT FOR THE CLICK TO HAPPEN AND THEN MOVE ON
-		
-		
-		
+		synchronized(GameDriver){
+			try{
+				this.sleep();
+			}
+			catch(Exception a){}
+		}
 		
 		
 		
